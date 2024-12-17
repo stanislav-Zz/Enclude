ac380740d1d8fe4761645b35f2440ed4ae3ef7de
Stanislav Zuev <zuev.stanislav@mail.ru>
2024-12-17 09:13:34 +0300

    Добавил картинок и галлерею

--- src/main/java/org/example/controller/ExampleController.java
+++ src/main/java/org/example/controller/ExampleController.java
@@ -20,4 +20,12 @@ public class ExampleController {
 
         return "home";
     }
+
+    @GetMapping("/gallery")
+    public String gallery(Model model) {
+        model.addAttribute("images", new String[]{
+                "image1.jpg", "image2.jpg", "image3.jpg", "image4.jpg", "image5.jpg", "image6.jpg"
+        });
+        return "gallery";
+    }
 }
